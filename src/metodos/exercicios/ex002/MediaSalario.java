6b9d1e716c4630c800bd786eb4e3c2f9b4ac6773
joaojorge <joaojorgedosanjos@gmail.com>
2023-07-11 22:41:12 -0300

    Aula métodos Senai exercicio 02 com a classe decimal format

--- src/metodos/exercicios/ex002/MediaSalario.java
+++ src/metodos/exercicios/ex002/MediaSalario.java
@@ -1,7 +1,11 @@
 package metodos.exercicios.ex002;
 
+import java.text.DecimalFormat;
+
 public class MediaSalario {
     public void calcularMediaSalarial(double salario, int count){
-        System.out.println("o Valor da média salarial é: " + (salario)/count);
+        DecimalFormat decimalFormat = new DecimalFormat("##,####.00");//ou "##,###.##"
+
+        System.out.println("o Valor da média salarial é:R$ " + decimalFormat.format(salario/count));
     }
 }
