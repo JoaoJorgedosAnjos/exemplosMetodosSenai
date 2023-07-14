7d9caf2704797e74bc9a18446898e26f2276d702
joaojorge <joaojorgedosanjos@gmail.com>
2023-07-13 23:16:58 -0300

    Refatorando o código

--- src/metodos/exercicios/ex002/MediaSalario.java
+++ src/metodos/exercicios/ex002/MediaSalario.java
@@ -3,7 +3,7 @@ package metodos.exercicios.ex002;
 import java.text.DecimalFormat;
 
 public class MediaSalario {
-    public void calcularMediaSalarial(double salario, int count){
+    protected void calcularMediaSalarial(double salario, int count){
         DecimalFormat decimalFormat = new DecimalFormat("##,####.00");//ou "##,###.##"
 
         System.out.println("o Valor da média salarial é:R$ " + decimalFormat.format(salario/count));
